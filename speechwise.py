ffc05aeb7a844b12053dc0c82438013334368d6e
pathikrit-repo-personal <pathikrit.sanyal@gmail.com>
2018-05-04 07:04:53 +0530

    add preliminary files to the project
    Signed-off-by: pathikrit sanyal <pathikrit.sanyal@gmail.com>

--- speechwise.py
+++ speechwise.py
@@ -1,19 +1,57 @@
 import speech_recognition as sr
 import openpyxl
 
+
+def getAudioInputFromUser(speechRecognizer, soundSource, row, column, excelWorkBook):
+
+    with soundSource as source:
+        audio = speechRecognizer.listen(source)
+    print ("input recieved")
+    text = speechRecognizer.recognize_google(audio)
+    excelSheet = excelWorkBook['Sheet1']
+    excelSheet.cell(row=row, column=column, value=text)
+    print(text)
+    excelWorkBook.save("Book1.xlsx")
+    return text
+
+
+
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Say Something!')
-    audio = r.listen(source)
-    print('Done!')
+# with sr.Microphone() as source:
+
 try:
-    text = r.recognize_google(audio)
-    print('Speech Recognizer thinks you said:\n' + text)
-    wb = openpyxl.load_workbook('C:\\Users\\pathikrit\\Desktop\\Book1.xlsx')
-    print (text)
+    varIdentifier = "start"
+    varExcelrow = 1
+    varExcelcolumn = 1
+    varIteration = 1
+    varMainChoices = ["given", "and", "when", "then", "wrap it up"]
+
+    wb = openpyxl.load_workbook(filename='C:\\Users\\pathikrit\\Desktop\\Book1.xlsx')
+    print("Welcome to the automated test creation application.")
+
+    while varIdentifier != "wrap it up":
+        print("select any one of the command by saying the command avilable commands are.\n given, and, when, then or just say wrap it up to finish")
+        print("Listening...\n")
+        resultCommand = getAudioInputFromUser(r, sr.Microphone(), varExcelrow, varExcelcolumn, wb)
+        if resultCommand.lower() in varMainChoices:
+            if resultCommand.lower() != "wrap it up":
+                varExcelcolumn = varExcelcolumn+1
+                # command is now in the excel file now we need a sentence for the command
+                print("say the sentence for the "+resultCommand+" Command")
+                print("Listening...\n")
+                resultDetail = getAudioInputFromUser(r, sr.Microphone(), varExcelrow, varExcelcolumn, wb)
+                print("you said "+resultDetail)
+                varExcelrow = varExcelrow+1
+                varExcelcolumn=1
+            else:
+                print("finish because user said wrap it up")
+                # take everything to .txt file except the last row where wrap it up is written.
+        else:
+            print("error because what user said is not among the commands")
+           # last input command was something wrong handle that atleast delete it from the workbook.
 
 
 except Exception as e:
-    print(e)
+    print (e)
 
